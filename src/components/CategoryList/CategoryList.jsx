7ae5755aadd9c53df2ec0d36b5b76c1395e21e51
Md Mohin Uddin <mdmohinuddin.cse@gmail.com>
2024-03-19 09:54:16 +0600

    project set up

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CategoryList = () => {
+  return (
+    <div>
+      <h2 className="text-6xl text-center">Job Category List</h2>
+      <p></p>
+    </div>
+  );
+};
+
+export default CategoryList;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+import CategoryList from "../CategoryList/CategoryList";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <CategoryList></CategoryList>
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
